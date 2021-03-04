2206f14db5244b3c9adf0e35154f506d94a51441
luciantin <lord66tin66@gmail.com>
2021-03-04 11:27:06 +0100

    do gradijenta

--- NetworkClasses/Layers.py
+++ NetworkClasses/Layers.py
@@ -10,3 +10,11 @@ class Layer_Dense:
     def forward(self, inputs):
         # calc ouput vals from inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    def backwar(self, dvalues):
+        # gradient on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
